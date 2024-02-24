7e9b7080bb2ad8d772c6542f0850c7fe79933181
Raul Glogovetan <raul@glogovetan.com>
2024-02-25 00:15:30 +0200

    Add: draft mode tag for articles

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -1,9 +1,11 @@
 import { Post } from "contentlayer/generated";
-import { Inter, Noto_Sans, Playfair_Display } from "next/font/google";
+import { Noto_Sans, Playfair_Display } from "next/font/google";
 
 import { Mdx } from "@components/mdx-components";
+import { PostStatus } from "@types";
 
 const playfair = Playfair_Display({ subsets: ["latin"] });
+const notoSans = Noto_Sans({ subsets: ["latin"] });
 
 type ExtendedPost = Post & {
   displayDate?: Date;
@@ -16,6 +18,11 @@ const Article = ({ post }: { post: ExtendedPost }) => (
         className={`max-w-3xl mx-auto mb-0 text-yellow-600 dark:text-yellow-700 font-semibold ${playfair.className}`}
       >
         {post.title}
+        {post.status === PostStatus.Draft && (
+          <span className={`align-middle ml-4 p-1 px-3 rounded-full text-sm text-zinc-200 dark:text-zinc-950 bg-gray-400 dark:bg-gray-600 ${notoSans.className}`}>
+            DRAFT
+          </span>
+        )}
       </h1>
       {post.description && (
         <p className="max-w-3xl mx-auto mb-0 mt-10 text-lg tracking-tight text-e-700 dark:text-slate-200">
