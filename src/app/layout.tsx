da4b87c659ed13ca0f2a5d02fcf74f3f16c93980
Raul Glogovetan <raul@glogovetan.com>
2024-02-25 22:06:50 +0200

    Refactor: improved homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import { ThemeProvider } from "@components/theme-provider";
 import { ModeToggle } from "@components/mode-toggle";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSans = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-slate-50 ${inter.className}`}
+        className={`antialiased min-h-screen bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-slate-50 ${notoSans.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
           <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 import { allPosts } from "contentlayer/generated";
 import Link from "next/link";
+import { Inter, Noto_Sans, Playfair_Display } from "next/font/google";
+
+import Article from "@components/article";
+import { PostStatus } from "@types";
+
+const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export default function Home() {
+  const sortedPosts = [...allPosts]
+    // filter by status (don't show drafts)
+    .filter(post => post.status !== PostStatus.Draft)
+    // sort by createdAt, updatedAt, or date
+    .sort((postA, postB) => {
+      const postADate = new Date(postA.updatedAt || postA.createdAt || postA.date || 0);
+      const postBDate = new Date(postB.updatedAt || postB.createdAt || postB.date || 0);
+      return postBDate.getTime() - postADate.getTime();
+    })
+    // combine dates into a single display date value
+    .map(post => {
+      const postDate = post.updatedAt || post.createdAt || post.date || 0;
+      const displayDate = new Date(postDate);
+      return {
+        ...post,
+        displayDate: postDate ? displayDate : undefined,
+      };
+    });
+
+  const latestPost = sortedPosts.shift();
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {latestPost  && <Article post={latestPost} />}
+
+      {(sortedPosts.length > 0) && (
+        <h2 className={`text-yellow-600 dark:text-yellow-700 ${playfair.className}`}>
+          Read More
+        </h2>
+      )}
+
+      {sortedPosts.map(post => (
         <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
+          <Link href={post.slug} className="no-underline hover:underline">
+            <h3 className="inline">{post.title}</h3>
           </Link>
           {post.description && <p>{post.description}</p>}
+          {post.displayDate && <p>{post.displayDate.toDateString()}</p>}
         </article>
       ))}
     </div>
